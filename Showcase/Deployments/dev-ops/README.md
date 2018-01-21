b8ddc02106e4ba29d221028866f2ece26554b53e
stefanhans <uni@stefan-hans.de>
2018-01-21 10:04:51 +0100

    Restructure Project

---
+++ Showcase/Deployments/dev-ops/README.md
@@ -0,0 +1,10 @@
+
+    
+Kubernetes: Deploy Production Webserver
+
+    
+
+Build and run Go executables
+
+    go build -o ./deploy_dev-ops deploy_dev-ops.go    
+    ./deploy_dev-ops
---
+++ Showcase/Images/dev-ops-webserver/buildandrun.sh
@@ -0,0 +1,19 @@
+#!/bin/sh +x
+
+ls -l main.go
+echo "curl https://raw.githubusercontent.com/stefanhans/Go4k8s/master/Showcase/Images/webserver/main.go > main.go"
+curl https://raw.githubusercontent.com/stefanhans/Go4k8s/master/Showcase/Images/webserver/main.go > main.go
+ls -l main.go
+
+echo "gofmt -w ."
+gofmt -w .
+
+echo "golint ./..."
+golint ./...
+
+echo "go build -o ./webserver main.go"
+go build -o ./webserver main.go
+
+echo "./webserver"
+./webserver
+
