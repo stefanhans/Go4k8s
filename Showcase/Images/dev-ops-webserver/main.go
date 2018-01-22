d2d2afe7e260bac882a28ced97209f266c3d0141
stefanhans <uni@stefan-hans.de>
2018-01-22 19:47:15 +0100

    Test version

---
+++ Showcase/Images/dev-ops-webserver/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+var html = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <title>Go4k8s</title>
+</head>
+<body style="background-color:yellow">
+  <h3>Hello From Go4k8s Webserver</h3>
+  <p>Version: %s</p>
+</body> 
+</html>
+`
+var version = "1.0.2"
+
+func httpHandler(w http.ResponseWriter, r *http.Request) {
+	format := "%s - [%s] \"%s %s %s\" %s\n"
+	fmt.Printf(format, version, time.Now().Format(time.RFC1123),
+		r.Method, r.URL.Path, r.Proto, r.UserAgent())
+	fmt.Fprintf(w, html, version)
+}
+
+func main() {
+
+	http.HandleFunc("/", httpHandler)
+
+	err := http.ListenAndServe(":8080", nil)
+
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
