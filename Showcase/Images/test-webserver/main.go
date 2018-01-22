da5322b41bf32a6adcca306a832045b70b0f2803
stefanhans <uni@stefan-hans.de>
2018-01-22 20:08:47 +0100

    Presenting and Testing

--- Showcase/Images/test-webserver/main.go
+++ Showcase/Images/test-webserver/main.go
@@ -14,7 +14,7 @@ var html = `
   <meta charset="utf-8" />
   <title>Go4k8s</title>
 </head>
-<body style="background-color:yellow">
+<body style="background-color:red">
   <h3>Hello From Go4k8s Webserver</h3>
   <p>Version: %s</p>
 </body>
